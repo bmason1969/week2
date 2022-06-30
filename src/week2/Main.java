f1598ddbf080d9b18ab97a7a81b8460ad5b93b7b
BRIAN MASON <bmasonn@yahoo.com>
2022-06-30 11:37:12 -0400

    added more code

--- src/week2/Main.java
+++ src/week2/Main.java
@@ -1,54 +1,97 @@
 package week2;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// conditional statements 
-		String name = "Sam";
+	//	int age = 14;
+		//System.out.println(age >=16);
+		//age = 17;
+		//System.out.println(age >=16);
 		
-		if (name == "Sam"){
-			System.out.println("Hello Sam!"); 
-		} else {
-			System.out.println("You are not Sam, " + name);
-		}	
-		
-		int age = 14;
-		
-		if (age >= 16) {
-			System.out.println("You can get your license");
-		} else {
-			//System.out.println("please wait until you are 16 years old");
-			System.out.println("please wait " + ( 16 - age ) + " year(s) to get your license");
-		}
-		double costOfMilk = 2;
-		
-		if (costOfMilk <= 2) {
-		System.out.println("buying 2");	
-		} else if (costOfMilk <=3) {
-			System.out.println("Buying 1");
-		} else {
-				System.out.println("Not buying any milk.");
-			}
-			char grade = 'C';
+//		boolean hasLicense = true;
+	//	if (age >= 16 && hasLicense) {
+		//	System.out.println("You can drive");
+	//} else {
+		//	System.out.println("You cannot drive");
 			
-			switch (grade) {
-			case 'A':
-				System.out.println("90%");
-				break;
-			case 'B':
-			System.out.println("80%");
-				break;
-			case 'C':
-				System.out.println("70%");
-				break;
-			default:
-				System.out.println("0%");
+		//	double costOfMilk = 3.5;
+			//int thirstLevel = 7;
 			
+			//if (costOfMilk < 2.5|| thirstLevel > 6) {
+				//System.out.println("Milk please");
+			//} else {
+				//	System.out.println("No thanks");
+					
+			int numberOfCookies = 17;
+			int numberOfChildren = 6;
+			
+			if (numberOfCookies % numberOfChildren == 0) {
+			 System.out.println("Sad face");
+			} else if (numberOfCookies % numberOfChildren < 2){
+				System.out.println("Yes");
+			} else if(numberOfCookies % numberOfChildren < 5) {
+				System.out.println("Whooohooo!");				
+			} else if (numberOfCookies % numberOfChildren >= 5) {
+				System.out.println("Jackpot!");
 			}
-		}
+					
+			String loyaltyMemberStatus = "GOLD";
+			double loyaltyMemberDiscount = 0.0;
+			
+			
+			switch (loyaltyMemberStatus){
+				case "SILVER":
+					loyaltyMemberDiscount= 0.10;
+					break;
+				case "GOLD":
+					loyaltyMemberDiscount = 0.15;
+					break;
+				case "PLATINUM":
+					loyaltyMemberDiscount = .025;
+					break;
+								
+			}
+			
+			System.out.println(loyaltyMemberDiscount);
 		
+	
+			
+		
+			
+			double billTotal = 785.50;
+			double adjustedTotal = billTotal - loyaltyMemberDiscount * billTotal;
+			System.out.println(adjustedTotal); 
+	
+	 	if (adjustedTotal > 500) {
+	   		if (loyaltyMemberStatus == "SILVER") {
+				loyaltyMemberStatus = "GOLD";
+	   		}else if (loyaltyMemberStatus == "GOLD") {
+				loyaltyMemberStatus = "PLATINUM";
+	   		}				
+	   	}
+	 	
+		System.out.println(loyaltyMemberStatus);
+			
+	
+	String username = "Tommy123";
+	String password = "12345";
+	
+	if (username.equals("Tommy123")&& password.equals("12345")){
+		System.out.println("login Successful");
+	} else {
+		System.out.println("access denied");
+		}
 	}
+}
+	
+		
+	
+	
+
+		
+
 	
 
 		
